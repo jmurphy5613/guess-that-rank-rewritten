a4c90a184c7e8179c5fb9dd9592ad943763b6b57
jmurphy5613 <jmurphy5613@gmail.com>
2023-09-02 15:42:07 -0400

    finished navbar visuals

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,10 +1,34 @@
 import styles from './Navbar.module.css'
+import Logo from '@/components/icons/Logo'
+
+const links = [
+    {
+        name: 'Games',
+        href: '/games'
+    },
+    {
+        name: 'Leaderboard',
+        href: '/leaderboard'
+    },
+    {
+        name: 'Submit Clip',
+        href: '/submit'
+    },
+]
 
 const Navbar = () => {
     return (
         <div className={styles.container}>
             <div className={styles["left"]}>
-
+                <div className={styles["logo-image-container"]}>
+                    <Logo stroke="#354DA1" />
+                </div>
+                <h1 className={styles.title}>GuessThatRank</h1>
+                <div className={styles.links}>
+                    {links.map((link, index) => (
+                        <a className={styles.link} href={link.href}>{link.name}</a>
+                    ))}
+                </div>
             </div>
             <div className={styles["auth-container"]}>
                 <button className={styles["signin-button"]}>Sign in</button>
