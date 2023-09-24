6b555a5bf5ab6d6ff640b0462f2e615c9aef849f
jmurphy5613 <jmurphy5613@gmail.com>
2023-09-24 17:33:44 -0400

    fixed silly import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,6 @@ import { ConvexProvider, ConvexReactClient } from "convex/react"
 import { useRouter } from "next/router"
 import { useEffect } from "react"
 import ReactGA from "react-ga4";
-import { Head } from "next/document"
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!)
 
